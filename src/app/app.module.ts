36bd88b576a21386da84e54374f8a5be0caf1e4d
InesRM <107609883+InesRM@users.noreply.github.com>
2023-01-17 12:20:21 +0100

    creación de módulos ( se ha hecho al revés)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroe/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
