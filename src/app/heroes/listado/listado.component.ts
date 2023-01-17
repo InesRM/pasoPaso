13bbf4e525ec857363fb79b05ae750281e9138ee
InesRM <107609883+InesRM@users.noreply.github.com>
2023-01-17 11:43:40 +0100

    *ngIf, *ngFor

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit{
+  heroeborrado: string = '';
+
   heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
 
 constructor() {
@@ -15,4 +17,10 @@ ngOnInit(): void {
   // se inicializan cosas
 console.log('entro en el onInit');
 }
+
+borrarHeroe(): void {
+  this.heroeborrado=this.heroes.shift() || '';
+  //pop para borrar el último
+}
+
 }
